19ca7b73a5a4d7c4a29dbb2b0a3f3518491075b2
xavi <xavi@delaguayaba.com>
2018-05-18 21:17:06 -0600

    Delete main spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,3 @@ require 'yaml'
 require File.expand_path("../../src/player", __FILE__)
 require File.expand_path("../../src/board", __FILE__)
 require File.expand_path("../../src/game_logic", __FILE__)
-require File.expand_path("../../src/main", __FILE__)
